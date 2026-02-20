9de67f16a48589297269fc474802b6dedffc90e1
JFree00 <121167426+JFree00@users.noreply.github.com>
2026-02-20 14:51:31 -0500

    improve

--- include/encryption.h
+++ include/encryption.h
@@ -2,9 +2,22 @@
 #define FIRST_C_PROJECT_ENCRYPTION_H
 #include <sodium.h>
 
-int hash(const char *password, char *out, size_t len);
-int check(const char *password, const char *hash, size_t len);
+#define SECURE_BUF_MAGIC 0x53454355u
 
-int createPassword(const char *input, char *out, size_t len);
+typedef struct secure_buf {
+  char *buf;
+  size_t len;
+  unsigned int magic;
+} secure_buf;
+
+int secure_buf_lock(secure_buf *sb, char *buf, size_t len);
+int secure_buf_unlock(secure_buf *sb);
+
+int hash(const char *password, char *out);
+int check(const char *password, const char *hash);
+int hash_secure(const secure_buf *sb, char *out);
+int check_secure(const secure_buf *sb, const char *hash);
+
+int createPassword(const char *input, char *out);
 
 #endif  // FIRST_C_PROJECT_ENCRYPTION_H
--- tests/test_encryption.c
+++ tests/test_encryption.c
@@ -19,39 +19,47 @@ static void enable_fast_pwhash_for_tests(void) {
 void test_hash() {
   char out[crypto_pwhash_STRBYTES];
   char password[124] = TEST_PASSWORD;
-  sodium_mlock(password, sizeof(password));
-  int res = hash(password, out, crypto_pwhash_STRBYTES);
+  secure_buf password_buf = {0};
+  TEST_ASSERT_EQUAL(0, secure_buf_lock(&password_buf, password, sizeof(password)));
+  int res = hash_secure(&password_buf, out);
+  TEST_ASSERT_EQUAL(0, secure_buf_unlock(&password_buf));
   TEST_ASSERT_EQUAL(0, res);
 }
 
 void test_check_hash_against_password_success() {
   char out[crypto_pwhash_STRBYTES];
   char password[124] = TEST_PASSWORD;
-  sodium_mlock(password, sizeof(password));
-  hash(password, out, sizeof(password));
-  // Password is now empty
-  // Re-init
-  char copyPassword[124] = TEST_PASSWORD;
+  secure_buf password_buf = {0};
+  TEST_ASSERT_EQUAL(0, secure_buf_lock(&password_buf, password, sizeof(password)));
+  hash_secure(&password_buf, out);
+  TEST_ASSERT_EQUAL(0, secure_buf_unlock(&password_buf));
 
-  int res = check(copyPassword, out, sizeof(password));
+  char copyPassword[124] = TEST_PASSWORD;
+  secure_buf copy_buf = {0};
+  TEST_ASSERT_EQUAL(0, secure_buf_lock(&copy_buf, copyPassword, sizeof(copyPassword)));
+  int res = check_secure(&copy_buf, out);
+  TEST_ASSERT_EQUAL(0, secure_buf_unlock(&copy_buf));
   TEST_ASSERT_EQUAL(0, res);
 }
 void test_check_hash_against_password_failure() {
   char out[crypto_pwhash_STRBYTES];
   char password[124] = TEST_PASSWORD;
-  sodium_mlock(password, sizeof(password));
-  hash(password, out, crypto_pwhash_STRBYTES);
+  secure_buf password_buf = {0};
+  TEST_ASSERT_EQUAL(0, secure_buf_lock(&password_buf, password, sizeof(password)));
+  hash_secure(&password_buf, out);
+  TEST_ASSERT_EQUAL(0, secure_buf_unlock(&password_buf));
 
   char wrong_password[124] = "WRONG PASSWORD";
-  int res = check(wrong_password, out, crypto_pwhash_STRBYTES);
+  secure_buf wrong_buf = {0};
+  TEST_ASSERT_EQUAL(0, secure_buf_lock(&wrong_buf, wrong_password, sizeof(wrong_password)));
+  int res = check_secure(&wrong_buf, out);
+  TEST_ASSERT_EQUAL(0, secure_buf_unlock(&wrong_buf));
   TEST_ASSERT_NOT_EQUAL(0, res);
 }
 
 void test_create_password() {
   char out[crypto_pwhash_STRBYTES];
-  char password[124] = TEST_PASSWORD;
-  sodium_mlock(password, sizeof(password));
-  int res = createPassword(password, out, crypto_pwhash_STRBYTES);
+  int res = createPassword(TEST_PASSWORD, out);
   TEST_ASSERT_EQUAL(0, res);
 }
 int main() {
